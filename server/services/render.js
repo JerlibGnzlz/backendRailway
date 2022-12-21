030af665f2ff45d238cfde150e497cea51acfffb
jerlibgnzlz <jerlibgv@gmail.com>
2022-12-21 15:28:43 -0300

    chance1 server

--- server/services/render.js
+++ server/services/render.js
@@ -5,7 +5,7 @@ exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
     // axios.get('http://localhost:3000/api/users')
     axios
-        .get(`${REACT_APP_BACKEND_URL}/api/users`)
+        .get(`${REACT_APP_BACKEND_URL}/users`)
         .then(function (response) {
             res.render("index", { users: response.data });
         })
