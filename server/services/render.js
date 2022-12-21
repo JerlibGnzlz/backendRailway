31eb9ea61465f0a21d7792f312a143e48bb6c165
jerlibgnzlz <jerlibgv@gmail.com>
2022-12-21 15:23:58 -0300

    chance server

--- server/services/render.js
+++ server/services/render.js
@@ -1,29 +1,32 @@
-const axios = require('axios');
-
+const axios = require("axios");
+const { REACT_APP_BACKEND_URL } = process.env;
 
 exports.homeRoutes = (req, res) => {
     // Make a get request to /api/users
-    axios.get('http://localhost:3000/api/users')
-        .then(function(response){
-            res.render('index', { users : response.data });
+    // axios.get('http://localhost:3000/api/users')
+    axios
+        .get(`${REACT_APP_BACKEND_URL}api/users`)
+        .then(function (response) {
+            res.render("index", { users: response.data });
         })
-        .catch(err =>{
+        .catch(err => {
             res.send(err);
+        });
+};
+
+exports.add_user = (req, res) => {
+    res.render("add_user");
+};
+
+exports.update_user = (req, res) => {
+    axios
+        .get("http://localhost:3000/api/users", {
+            params: { id: req.query.id },
         })
-
-    
-}
-
-exports.add_user = (req, res) =>{
-    res.render('add_user');
-}
-
-exports.update_user = (req, res) =>{
-    axios.get('http://localhost:3000/api/users', { params : { id : req.query.id }})
-        .then(function(userdata){
-            res.render("update_user", { user : userdata.data})
+        .then(function (userdata) {
+            res.render("update_user", { user: userdata.data });
         })
-        .catch(err =>{
+        .catch(err => {
             res.send(err);
-        })
-}
+        });
+};
